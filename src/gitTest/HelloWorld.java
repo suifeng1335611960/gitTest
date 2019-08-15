f97790563a5ba40b4e063e4082f88051eed1a3c6
qiang <qiang@qq.com>
2019-08-15 19:57:02 +0800

    first commit in eclipse.

---
+++ src/gitTest/HelloWorld.java
@@ -0,0 +1,16 @@
+package gitTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		System.out.println("hello world2");
+		
+		System.out.println("hello world3");
+		
+	}
+	
+	
+	
+}
